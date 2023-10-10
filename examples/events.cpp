e3966e8cb182eed3532d049c5fcc73c2be718560
Dian-Lun Lin <cyc4542000@gmail.com>
2023-10-10 14:45:26 -0500

    add events example

---
+++ examples/events.cpp
@@ -0,0 +1,28 @@
+
+#include <taro.hpp>
+#include <taro/scheduler/events.hpp>
+#include <vector>
+#include <algorithm>
+
+int main() {
+  size_t NUM_THREADS{2};
+  size_t NUM_EVENTS{4};
+
+  taro::Taro taro{NUM_THREADS};
+  auto events = taro.event_scheduler(NUM_EVENTS); // create four events
+
+  for(size_t i = 0; i < NUM_EVENTS; ++i) {
+    taro.emplace([&, i]() {
+      std::cout << "setting event" << i << "\n";
+      events.set(i); // set event i
+    });
+
+    taro.emplace([&, i]() -> taro::Coro {
+      co_await events.until(i); // wait until event i is set
+      std::cout << "event " << i << "is set\n";
+    });
+  }
+
+  taro.schedule();
+  taro.wait();
+}
