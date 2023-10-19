811a9bbb27054539388aa2bc981cfc85be00cd2b
Dian-Lun Lin <cyc4542000@gmail.com>
2023-10-19 15:25:35 -0500

    update simpe.cu example

--- examples/simple.cu
+++ examples/simple.cu
@@ -1,56 +1,46 @@
 #include <taro.hpp>
-#include <taro/scheduler/cuda.hpp>
+#include <taro/await/cuda.hpp>
 
 int main() {
   taro::Taro taro{4}; // number of threads
-  auto cuda = taro.cuda_scheduler(4); // number of cuda streams
-  int* d_a, *d_b, *d_c;
-  std::vector<int> h_c;
+  auto cuda = taro.cuda_await(4); // number of cuda streams
+  int* d_a;
   size_t N{100};
+  size_t BLOCK_SIZE{128};
+
+  std::vector<int> h_a(N * N);
+  std::vector<int> h_b(N * N);
+  dim3 dim_grid((N - 1) / BLOCK_SIZE + 1, (N - 1) / BLOCK_SIZE + 1, 1);
+  dim3 dim_block(BLOCK_SIZE, BLOCK_SIZE, 1);
+
+  auto task_a = taro.emplace([&]() {
+    std::iota(h_a.begin(), h_a.end(), 0);
+    std::cout << "task a\n";
+  });
+  
+  // malloc
+  auto task_b = taro.emplace([=, &cuda]() {
+    cuda.wait([=](cudaStream_t stream) {  // wait method
+      cudaMallocAsync((void**)&d_a, N * N * sizeof(int), stream);
+    });
+    std::cout << "task b use wait methodg\n";
+  });
   
   // H2D
-  auto task_a = taro.emplace([&cuda, d_a, N]() -> taro::Coro {
-    std::vector<int> h_a(N * N);
-    std::iota(h_a.begin(), h_a.end(), 0);
-    co_await cuda.suspend_polling([&d_a, &h_a, N](cudaStream_t stream) {  // polling method
-      cudaMallocAsync((void**)&d_a, N * N * sizeof(int), stream);
+  auto task_c = taro.emplace([=, &h_a, &cuda]() -> taro::Coro {
+    co_await cuda.until_polling([=, &h_a](cudaStream_t stream) { // polling method   
       cudaMemcpyAsync(d_a, h_a.data(), N * N * sizeof(int), cudaMemcpyHostToDevice, stream);
     });
-    std::cout << "task a polling\n";
+    std::cout << "task c use polling method\n";
   });
   
-  // H2D
-  auto task_b = taro.emplace([&cuda, d_b, N]() -> taro::Coro {
-    std::vector<int> h_b(N * N);
-    std::iota(h_b.begin(), h_b.end(), 0);
-    co_await cuda.suspend_polling([&d_b, &h_b, N](cudaStream_t stream) {  // polling method  
-      cudaMallocAsync((void**)&d_b, N * N * sizeof(int), stream);
-      cudaMemcpyAsync(d_b, h_b.data(), N * N * sizeof(int), cudaMemcpyHostToDevice, stream);
-    });
-    std::cout << "task b polling\n";
-  });
-  
-  // D2H
-  auto task_c = taro.emplace([&cuda, &h_c, d_c, N]() -> taro::Coro {
-    size_t BLOCK_SIZE{128};
-    dim3 dim_grid((N - 1) / BLOCK_SIZE + 1, (N - 1) / BLOCK_SIZE + 1, 1);
-    dim3 dim_block(BLOCK_SIZE, BLOCK_SIZE, 1);
-    h_c.resize(N * N);
-    co_await cuda.suspend_callback([=, &d_c, &h_c](cudaStream_t stream) { // callback method   
-      cudaMallocAsync((void**)&d_c, N * N * sizeof(int), stream);
-      cudaMemcpyAsync(h_c.data(), d_c, N * N * sizeof(int), cudaMemcpyDeviceToHost, stream);
-    });
-    std::cout << "task c callback\n";
-  });
-  
-  // free
-  auto task_d = taro.emplace([&cuda, d_a, d_b, d_c]() { 
-    cuda.wait([=](cudaStream_t stream) {   // synchronous 
+  // D2H and free
+  auto task_d = taro.emplace([=, &h_b, &cuda]() -> taro::Coro { 
+    co_await cuda.until_callback([=, &h_b](cudaStream_t stream) {   // polling method
+      cudaMemcpyAsync(h_b.data(), d_a, N * N * sizeof(int), cudaMemcpyDeviceToHost, stream);
       cudaFreeAsync(d_a, stream);
-      cudaFreeAsync(d_b, stream);
-      cudaFreeAsync(d_c, stream);
     });
-    std::cout << "task d wait\n";
+    std::cout << "task d use callback method\n";
   });
   
   // dependency
