73e0e41af507b534bd4b20f510166b40f350545b
dian-lun-lin <cyc4542000@gmail.com>
2023-10-22 19:11:13 -0600

    update event example

--- examples/event.cpp
+++ examples/event.cpp
@@ -1,6 +1,6 @@
 
 #include <taro.hpp>
-#include <taro/scheduler/events.hpp>
+#include <taro/await/event.hpp>
 #include <vector>
 #include <algorithm>
 
@@ -9,17 +9,17 @@ int main() {
   size_t NUM_EVENTS{4};
 
   taro::Taro taro{NUM_THREADS};
-  auto events = taro.event_scheduler(NUM_EVENTS); // create four events
+  auto events = taro.event_await(NUM_EVENTS); // create four events
 
   for(size_t i = 0; i < NUM_EVENTS; ++i) {
     taro.emplace([&, i]() {
-      std::cout << "setting event" << i << "\n";
+      std::cout << "setting event " << i << "\n";
       events.set(i); // set event i
     });
 
     taro.emplace([&, i]() -> taro::Coro {
       co_await events.until(i); // wait until event i is set
-      std::cout << "event " << i << "is set\n";
+      std::cout << "event " << i << " is set\n";
     });
   }
 
